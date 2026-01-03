27e360b8a1e587d1ea14a7f51f2c957cc308a808
SoumenBaral <shumen.sb@gmail.com>
2026-01-03 09:50:11 +0600

    add a user table

--- 3.Database/PDO/p1.php
+++ 3.Database/PDO/p1.php
@@ -1,3 +1,19 @@
 <?php
-$dsn ="sqlite:data.db";
-$pdo = new PDO($dsn);
+$dsn ="sqlite:data.db";#Dsn means Data Source Name
+try{
+    $pdo = new PDO($dsn);
+    $pdo-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql ="CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        gender VARCHAR(10) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )";
+    $pdo->exec($sql);
+    echo "Table users created successfully";
+}
+catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+};

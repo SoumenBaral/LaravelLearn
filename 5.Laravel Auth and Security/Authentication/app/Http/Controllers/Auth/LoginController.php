a079a78d066d9b112227e3a942bdf8581de73850
Soumen Baral <shumen.sb@gmail.com>
2026-02-06 10:09:54 +0600

    add login Resister

--- 5.Laravel Auth and Security/Authentication/app/Http/Controllers/Auth/LoginController.php
+++ 5.Laravel Auth and Security/Authentication/app/Http/Controllers/Auth/LoginController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     function show()
     {
         return view('auth.login');
+    }
+    public function store(Request $request){
+        //1. Validate The Requested data (email should be unique)
+        //2. Authenticate the user
+        //3. Redirect to the Dashboard page
+        $request->validate([
+            'email'=>'required|email|exists:users,email',
+            'password'=>'required'
+        ]);
+        $user = User::where('email',$request->email)->first();
+        if(!$user || !\Hash::check($request->password,$user->password)){
+            return back()->withErrors(['email'=>'The provided credentials are incorrect']);
+        }
+        Auth::login($user,$request->boolean('remember'));
+        return redirect()->route('dashboard');
+
     }
 }
--- 5.Laravel Auth and Security/Authentication/app/Http/Controllers/Auth/RegisterController.php
+++ 5.Laravel Auth and Security/Authentication/app/Http/Controllers/Auth/RegisterController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
     public function show(){
         return view('auth.register');
+    }
+    public function store(Request $request){
+
+        //1. Validate The Requested data (email should be unique)
+        //2. Create a new user record in the database
+        //3.1(Optional) Redirect to the login page
+        //3.2(Optional)After a new user, we can authenticate the user automatically and Redirect to the Dashboard page.
+
+        $request->validate([
+            'name' => 'required|string|min:3|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:8|confirmed',
+        ]);
+        $user=User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => ($request->input('password')),
+        ]);
+        return redirect()->route('login');
     }
 }

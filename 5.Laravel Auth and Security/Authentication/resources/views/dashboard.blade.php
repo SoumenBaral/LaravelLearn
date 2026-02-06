3c897d99ab1e555bafdae973c79af0d5d4ce5455
Soumen Baral <shumen.sb@gmail.com>
2026-02-06 10:22:37 +0600

    add login Resister

--- 5.Laravel Auth and Security/Authentication/resources/views/dashboard.blade.php
+++ 5.Laravel Auth and Security/Authentication/resources/views/dashboard.blade.php
@@ -1,3 +1,13 @@
 <div>
     <h2>Dashboard</h2>
+    <p>Welcome {{auth()->user()->name??'User'}}</p>
+    <form action="{{route('logout')}}" method="post">
+        @csrf
+        <button type="submit">Logout</button>
+    </form>
+    <div>
+        <livewire:counter />
+
+    </div>
+
 </div>
